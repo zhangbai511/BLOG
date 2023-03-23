7ed188fee0fae4048be1f1bbea964e7021327a59
baisemede <1046975490@qq.com>
2023-03-23 14:20:00 +0800

    修改配置

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Air Blog`;
+export const SITE_TITLE = `日常随想`;
 export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_EMAIL = 'xxx.xxx@qq.com'
+export const SITE_NAME = 'tsytrjyn.vercel.app';
+export const SITE_URL = "https://tsytrjyn.vercel.app";
